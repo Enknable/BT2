304f36ca1e763ccba195d1051765744d7a43e6d0
Andrew <lil_tagger04@hotmail.com>
2013-11-03 10:07:43 -0500

    hi

--- bt.c
+++ bt.c
@@ -473,7 +473,7 @@ if(FD_ISSET(i, &write_fds)){
                 FD_CLR(sockfd, &master);
             
         fp2 = open("Newfile", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-        posix_fallocate(fp2, 0, 36);
+        posix_fallocate(fp2, 0, BT.sz);
         
 }
 printf("listenerUDP: waiting to recvfrom...\n");

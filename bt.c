33cb0a38026c03020d1e9f7da3e9de9f3bddd79b
Andrew <lil_tagger04@hotmail.com>
2013-11-03 10:43:55 -0500

    hi

--- bt.c
+++ bt.c
@@ -500,8 +500,8 @@ printf("listenerUDP: waiting to recvfrom...\n");
         sprintf(buf2+(2*q), "%02x", digest[q]);
     }
     
-    if(strncmp (buf1, buf2, 16) != 0)
-    continue;
+    //if(strncmp (buf1, buf2, 16) != 0)
+    //continue;
     printf("listenerUDP: got packet from %s\n",
         inet_ntop(their_addr2.ss_family,
             get_in_addr((struct sockaddr *)&their_addr2),
